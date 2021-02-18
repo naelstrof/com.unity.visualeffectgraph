7246d4a7c17e8883c75c26caf8a64063d5174ccf
Naelstrof <naelstrof@gmail.com>
2021-02-18 00:04:15 -0700

    Janky support for Collision With Depth block on URP

---
+++ Editor/Utilities/PropertyBinding/VFXDepthBinder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace UnityEngine.VFX.Utility
+{
+    [AddComponentMenu("VFX/Property Binders/Depth Binder")]
+    [VFXBinder("Depth/Depth")]
+    class VFXDepthBinder : VFXBinderBase {
+        public string Property { get { return (string)m_Property; } set { m_Property = value; UpdateSubProperties(); } }
+        [VFXPropertyBinding("UnityEditor.VFX.Depth"), SerializeField, UnityEngine.Serialization.FormerlySerializedAs("m_Parameter")]
+        protected ExposedProperty m_Property = "DepthTexture";
+        private ExposedProperty DepthBuffer;
+        private Texture depthTexture;
+        protected override void OnEnable() {
+            base.OnEnable();
+            UpdateSubProperties();
+        }
+
+        void OnValidate() {
+            UpdateSubProperties();
+        }
+
+        void UpdateSubProperties() {
+            DepthBuffer = m_Property;
+        }
+
+        public override bool IsValid(VisualEffect component) {
+            return component.HasTexture((int)DepthBuffer);
+        }
+
+        public override void UpdateBinding(VisualEffect component) {
+            if (depthTexture == null) {
+                depthTexture = Shader.GetGlobalTexture("_CameraDepthTexture");
+            }
+            component.SetTexture((int)DepthBuffer, depthTexture);
+        }
+
+        public override string ToString() {
+            return string.Format("Depth : '{0}' -> {1}", m_Property, depthTexture == null ? "(null)" : depthTexture.ToString());
+        }
+    }
+}
